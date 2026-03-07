67e73eef945329377777f68022a415936cbd7491
tibertun <tominasonyaa@gmail.com>
2026-03-07 14:47:59 +0200

    dev5: add is_power_of_5 function

--- Desktop/lab5/lab5.2.2/main.py
+++ Desktop/lab5/lab5.2.2/main.py
@@ -2,3 +2,6 @@ from utils import factorial, is_prime
 
 print("Factorial of 5:", factorial(5))
 print("Is 7 prime?", is_prime(7))
+
+from utils import is_power_of_5
+print("Is 125 power of 5?", is_power_of_5(125))
--- Desktop/lab5/lab5.2.2/utils.py
+++ Desktop/lab5/lab5.2.2/utils.py
@@ -10,3 +10,10 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+def is_power_of_5(n):
+    if n < 1:
+        return False
+    while n % 5 == 0:
+        n //= 5
+    return n == 1
