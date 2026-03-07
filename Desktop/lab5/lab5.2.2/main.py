808447c7869534a106e787d6596a420451e6cf07
tibertun <tominasonyaa@gmail.com>
2026-03-07 14:49:59 +0200

    main: add is_power_of_2 function

--- Desktop/lab5/lab5.2.2/main.py
+++ Desktop/lab5/lab5.2.2/main.py
@@ -2,3 +2,12 @@ from utils import factorial, is_prime
 
 print("Factorial of 5:", factorial(5))
 print("Is 7 prime?", is_prime(7))
+<<<<<<< Updated upstream
+=======
+
+from utils import is_power_of_5
+print("Is 125 power of 5?", is_power_of_5(125))
+
+from utils import is_power_of_2
+print("Is 64 power of 2?", is_power_of_2(64))
+>>>>>>> Stashed changes
--- Desktop/lab5/lab5.2.2/utils.py
+++ Desktop/lab5/lab5.2.2/utils.py
@@ -10,3 +10,18 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+<<<<<<< Updated upstream
+=======
+
+def is_power_of_5(n):
+    if n < 1:
+        return False
+    while n % 5 == 0:
+        n //= 5
+    return n == 1
+
+def is_power_of_2(n):
+    if n < 1:
+        return False
+    return (n & (n - 1)) == 0
+>>>>>>> Stashed changes
