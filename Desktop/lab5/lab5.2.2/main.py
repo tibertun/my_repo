02fb83fe27dce65ee3f6949633b1a589bd6cf243
tibertun <tominasonyaa@gmail.com>
2026-03-07 14:46:47 +0200

    Add utils.py with factorial and is_prime, add main.py

---
+++ Desktop/lab5/lab5.2.2/main.py
@@ -0,0 +1,4 @@
+from utils import factorial, is_prime
+
+print("Factorial of 5:", factorial(5))
+print("Is 7 prime?", is_prime(7))
---
+++ Desktop/lab5/lab5.2.2/utils.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
